87e0f44565ed5dbaf59b6ca0031f68efd173fdec
Wainaina Gathecha <wainainagathecha@gmail.com>
2026-04-09 12:57:34 +0300

    build radial gradient mouse hover effect

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('mousemove', function(e){
+    document.documentElement.style.setProperty('--mouse-x', e.clientX + 'px');
+    document.documentElement.style.setProperty('--mouse-y', e.clientY + 'px');
+});
